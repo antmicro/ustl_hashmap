461a3c010fbbe70ea5e089d17d796d930c77770d
Łukasz Kucharski <lkucharski@antmicro.com>
2015-03-15 16:16:11 +0100

    Changed default hashing algorithm to Murmur264.
    Not every platform supports 128bit integers, so CrapWow64 was
    unsuitable. The default algorithm is changed. CrapWow is compiled with a
    preprocessor condition that checks whether the 128bit exstension is
    avaiable.
    
    Changed `size_type` to `ustl` (using local namespace) instead of `std`.

--- hash_functions.h
+++ hash_functions.h
@@ -2,6 +2,8 @@
 
 typedef uint64_t u64;
 typedef uint8_t u8;
+
+#ifdef __SIZEOF_INT128__
 __extension__ typedef unsigned __int128 u128;
 
 inline u64 CrapWow64( const u8 *key, u64 len, u64 seed ) {
@@ -19,4 +21,34 @@ inline u64 CrapWow64( const u8 *key, u64 len, u64 seed ) {
 	if ( len ) { cwmixa( key8[0] & ( ( (u64)1 << ( len * 8 ) ) - 1 ) ) }
 	cwmixb( h ^ (k + n) )
 	return k ^ h;
+}
+#endif
+
+inline u64 Murmur264( const u8 *key, u64 len, u64 seed ) {
+	const u64 m = 0xc6a4a7935bd1e995;
+	const u8 r = 47;
+	u64 h = len + seed;
+
+	for ( ; len >= 8; len -= 8, key += 8 ) {
+		u64 k = *(u64 *)key * m;
+		k ^= k >> r; 
+		k *= m;
+		h = ( h * m ) ^ k;
+	}
+
+	switch ( len & 7 ) {
+		case 7: h ^= u64(key[6]) << 48;
+		case 6: h ^= u64(key[5]) << 40;
+		case 5: h ^= u64(key[4]) << 32;
+		case 4: h ^= u64(key[3]) << 24;
+		case 3: h ^= u64(key[2]) << 16;
+		case 2: h ^= u64(key[1]) << 8;
+		case 1: h ^= u64(key[0]);
+	        h *= m;
+	}
+
+	h ^= h >> r;
+	h *= m;
+	h ^= h >> r;
+	return h;
 }
