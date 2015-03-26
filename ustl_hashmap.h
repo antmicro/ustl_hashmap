9c57a8184b4515c2d3c85bbfe0654d2418192bcd
Łukasz Kucharski <lkucharski@antmicro.com>
2015-03-26 16:35:50 +0100

    Formatting.

--- ustl_hashmap.h
+++ ustl_hashmap.h
@@ -15,47 +15,47 @@ namespace ustl {
   > 
   class unordered_map {
   public:
-    typedef Key 	key_type;
-    typedef T 		mapped_type;
-    typedef KeyEqual	key_comparer;
-    typedef pair<Key, T>		value_type;
-    typedef memblock::size_type		size_type;
-    typedef memblock::difference_type	difference_type;
-    typedef value_type&		reference;
-    typedef const value_type&	const_reference;
-    typedef const key_type&	const_key_ref;
-    typedef value_type*		pointer;
-    typedef const value_type*	const_pointer;
-    typedef pointer		iterator;
-    typedef const_pointer	const_iterator;
-    typedef ::ustl::reverse_iterator<iterator>	reverse_iterator;
+    typedef Key         key_type;
+    typedef T           mapped_type;
+    typedef KeyEqual    key_comparer;
+    typedef pair<Key, T>                value_type;
+    typedef memblock::size_type         size_type;
+    typedef memblock::difference_type   difference_type;
+    typedef value_type&         reference;
+    typedef const value_type&   const_reference;
+    typedef const key_type&     const_key_ref;
+    typedef value_type*         pointer;
+    typedef const value_type*   const_pointer;
+    typedef pointer             iterator;
+    typedef const_pointer       const_iterator;
+    typedef ::ustl::reverse_iterator<iterator>  reverse_iterator;
     typedef ::ustl::reverse_iterator<const_iterator> const_reverse_iterator;
-    typedef pair<iterator,bool>	insertrv_t;
-    typedef const unordered_map<Key, T, Hash, KeyEqual>&	crself_t;
+    typedef pair<iterator,bool> insertrv_t;
+    typedef const unordered_map<Key, T, Hash, KeyEqual>&        crself_t;
     
   public:
     unordered_map();
     unordered_map(size_type bucket_count);
-    inline mapped_type&		operator[] (const_key_ref key);
-    //inline const_reference 	operator[] (const_key_ref key) const;
-    insertrv_t			insert (const_reference v);
-    inline const_iterator	find (const_key_ref k) const;
-    inline iterator		find (const_key_ref k)	{ return const_cast<iterator> (const_cast<crself_t>(*this).find (k)); }
-    inline iterator		begin ()	{ assert(false); /* "Iterating over unordered_map is not implemented." */ };
-    inline const_iterator	end () const	{ return (const_iterator)_buckets.end(); };
-    inline iterator		end () 		{ return const_cast<iterator> (const_cast<crself_t>(*this).end ()); };
+    inline mapped_type&         operator[] (const_key_ref key);
+    //inline const_reference    operator[] (const_key_ref key) const;
+    insertrv_t                  insert (const_reference v);
+    inline const_iterator       find (const_key_ref k) const;
+    inline iterator             find (const_key_ref k)  { return const_cast<iterator> (const_cast<crself_t>(*this).find (k)); }
+    inline iterator             begin ()        { assert(false); /* "Iterating over unordered_map is not implemented." */ };
+    inline const_iterator       end () const    { return (const_iterator)_buckets.end(); };
+    inline iterator             end ()          { return const_cast<iterator> (const_cast<crself_t>(*this).end ()); };
     inline size_type            capacity() const    { return bucket_count(); };
-    inline size_type		bucket_count() const 	{ return _buckets.size()-H; };
-           void                 rehash(size_type new_bucket_count);
+    inline size_type            bucket_count() const    { return _buckets.size()-H; };
+            void                rehash(size_type new_bucket_count);
   protected:
-    typedef value_type  	bucket_type;
-    typedef size_type		bucket_key;
+    typedef value_type          bucket_type;
+    typedef size_type           bucket_key;
     typedef vector<bucket_type>	bucket_container;
-    typedef typename bucket_container::iterator		bucket_iterator;
-    typedef typename bucket_container::const_iterator	bucket_const_itr;
+    typedef typename bucket_container::iterator         bucket_iterator;
+    typedef typename bucket_container::const_iterator   bucket_const_itr;
     
   protected:
-    inline size_type    bucket(const_key_ref key) const 	{ return _hash(key) % bucket_count(); };
+    inline size_type    bucket(const_key_ref key) const { return _hash(key) % bucket_count(); };
 
   private:
     bucket_key          find_bucket(const_key_ref key, bucket_key start = 0) const;
